149ee569c3cf1bafbd0f08c76cb00b107b9f8a13
Sherlock55561 <105907814+Sherlock55561@users.noreply.github.com>
2025-03-27 12:21:43 +1100

    Update builder.py

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -25,7 +25,10 @@ from llava.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, D
 
 def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="auto", device="cuda", use_flash_attn=False, **kwargs):
     kwargs = {"device_map": device_map, **kwargs}
-
+    
+    if not hasattr(model.config, "attention_dropout"):
+    model.config.attention_dropout = 0.1
+    
     if device != "cuda":
         kwargs['device_map'] = {"": device}
 
